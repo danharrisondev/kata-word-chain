80b12922b1fc6aab2336ec8e3d6e4b147abfb6dd
Daniel Harrison <harrison92d+orangudan@gmail.com>
2017-01-26 20:55:19 +0000

    Func to test varied length words are similar (same word but with an additional character at either end)

--- src/App/Text.hs
+++ src/App/Text.hs
@@ -2,9 +2,16 @@ module App.Text where
 
 similar word compareWord | word == compareWord = False
 similar word compareWord =
-    variesByChars word compareWord
+    variesBySize word compareWord || variesByChars word compareWord
 
-variesByChars w1 w2 = characterDifference w1 w2 == 1
+variesByChars w1 w2 =
+    characterDifference w1 w2 == 1 && lengthDifference w1 w2 == 0
+
+variesBySize w1 w2 =
+    lengthDifference w1 w2 == 1 && characterDifference w1 w2 == 0
     
 characterDifference word compareWord =
     length $ filter (==False) $ zipWith (==) word compareWord
+
+lengthDifference word compareWord =
+    abs $ length word - length compareWord
